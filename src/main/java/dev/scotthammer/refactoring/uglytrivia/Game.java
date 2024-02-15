15812a9b2dc85e5d88d5f27b044b1114e9b1b832
Scott Hammer <shammer64@gmail.com>
2024-02-15 16:36:36 -0500

    Inlined createRockQuestion method, safe delete, removed redundant parens

--- src/main/java/dev/scotthammer/refactoring/uglytrivia/Game.java
+++ src/main/java/dev/scotthammer/refactoring/uglytrivia/Game.java
@@ -25,14 +25,10 @@ public class Game {
     public  Game(){
         for (int i = 0; i < 50; i++) {
             popQuestions.addLast("Pop Question " + i);
-            scienceQuestions.addLast(("Science Question " + i));
-            sportsQuestions.addLast(("Sports Question " + i));
-            rockQuestions.addLast(createRockQuestion(i));
+            scienceQuestions.addLast("Science Question " + i);
+            sportsQuestions.addLast("Sports Question " + i);
+            rockQuestions.addLast("Rock Question " + i);
         }
-    }
-
-    public String createRockQuestion(int index){
-        return "Rock Question " + index;
     }
 
     public boolean isPlayable() {
